696a42138bad0428dbc90dfc5d61859d85a935c5
MD Akash islam <52877878+akashislam90@users.noreply.github.com>
2022-01-27 20:12:59 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+document.querySelector('h1').style.fontSize = '50px'
+document.querySelector('h1').style.color = 'red'
+document.querySelector('h1').style.backgroundColor = 'black'
+document.querySelector('h1').style.padding = '100px'
+
+
+document.querySelector('p').style.fontSize = '30px'
+document.querySelector('p').style.color = 'black'
+document.querySelector('p').style.backgroundColor = 'pink'
+document.querySelector('p').style.padding = '100px'
+
+document.querySelector('h2').style.fontSize = '30px'
+document.querySelector('h2').style.color = '#00ff00'
+document.querySelector('h2').style.backgroundColor = '#0000FF'
+document.querySelector('h2').style.padding = '100px'
